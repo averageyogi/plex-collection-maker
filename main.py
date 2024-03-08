07a2b1a4ccde3c68f305de30d3fb1f501c863d25
averageyogi <52513521+averageyogi@users.noreply.github.com>
2024-03-08 00:19:01 -0600

    Add configuration example templates

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-import sys
 import os
-# import json
+import sys
 
-import requests
-# from bs4 import BeautifulSoup
-from plexapi.server import PlexServer
-import plexapi.exceptions
-from plexapi.library import LibrarySection
-# import time
-# import re
 from dotenv import load_dotenv
+import plexapi.exceptions
+from plexapi.server import PlexServer
+from plexapi.library import LibrarySection
+import requests
+import yaml
 
 
-load_dotenv()  # take environment variables from .env.
+load_dotenv(override=True)  # Take environment variables from .env
 
 class PlexCollectionMaker:
     """
@@ -20,18 +17,18 @@ class PlexCollectionMaker:
     """
     def __init__(self):
         self.load_config()
+        self.plex_setup()
 
     def load_config(self) -> None:
         """
-        Load environment variables from .env config.
+        Load environment variables from .env and library configuration from config.yml.
         """
         self.using_public_ip = False
         try:
             self.plex_token = os.environ["PLEX_TOKEN"]
-            self.tv_library = os.environ["PLEX_TV_LIBRARY"]
-            self.movie_library = os.environ["PLEX_MOVIE_LIBRARY"]
         except KeyError:
-            sys.exit("Error with .env file. Please consult the README.")
+            sys.exit('Cannot find "PLEX_TOKEN" in .env file. Please consult the README.')
+
         try:
             self.plex_ip = os.environ["PLEX_SERVER_IP"]
         except KeyError:
@@ -40,7 +37,7 @@ class PlexCollectionMaker:
                 self.plex_ip = os.environ["PLEX_SERVER_PUBLIC_IP"]
                 self.using_public_ip = True
             except KeyError:
-                sys.exit("Error with .env file. Please consult the README.")
+                sys.exit("Cannot find IP address in .env file. Please consult the README.")
         try:
             if not self.using_public_ip:
                 self.plex_pub_ip = os.environ["PLEX_SERVER_PUBLIC_IP"]
@@ -50,25 +47,39 @@ class PlexCollectionMaker:
             # Only local ip given
             self.plex_pub_ip = None
 
-    def plex_setup(self) -> tuple[LibrarySection, LibrarySection]:
-        """
-        Load PlexAPI config.
+        with open("./config.yml", encoding="utf-8") as config_file:
+            try:
+                config_yaml = yaml.safe_load(config_file)
+            except yaml.YAMLError as err:
+                print(err)
+        self.libraries = [*config_yaml['libraries']]
 
-        Returns:
-            (tv, movies): tv and movie LibrarySection objects
+        self.collections_config = {}
+        for lib in config_yaml['libraries']:
+            for coll_file in config_yaml['libraries'][lib]['collection_files']:
+                with open(coll_file['file'], encoding="utf-8") as collection_config_file:
+                    try:
+                        colls = yaml.safe_load(collection_config_file)
+                        self.collections_config[lib] = colls['collections']
+                    except yaml.YAMLError as err:
+                        print(err)
+
+    def plex_setup(self) -> None:
+        """
+        Load PlexAPI config and connect to server.
         """
         try:
-            plex = PlexServer(self.plex_ip, self.plex_token)
+            self.plex = PlexServer(self.plex_ip, self.plex_token)
         except requests.exceptions.InvalidURL:
-            sys.exit('Invalid IP address. Please check the server IP addresses in .env, and consult the README.')
+            sys.exit("Invalid IP address. Please check the server IP addresses in .env, and consult the README.")
         except requests.exceptions.RequestException:
             if self.plex_pub_ip:
                 try:
-                    plex = PlexServer(self.plex_pub_ip, self.plex_token)
+                    self.plex = PlexServer(self.plex_pub_ip, self.plex_token)
                 except requests.exceptions.RequestException:
                     sys.exit(
-                        'Unable to connect to Plex server. Please check the server '
-                        'IP addresses in .env, and consult the README.'
+                        "Unable to connect to Plex server. Please check the server "
+                        "IP addresses in .env, and consult the README."
                     )
                 except plexapi.exceptions.Unauthorized:
                     sys.exit(
@@ -76,36 +87,56 @@ class PlexCollectionMaker:
                     )
             else:
                 sys.exit(
-                    'Unable to connect to Plex server. Please check the '
-                    f'{"PLEX_SERVER_PUBLIC_IP" if self.using_public_ip else "PLEX_SERVER_IP"} '
-                    'in .env, and consult the README.'
+                    "Unable to connect to Plex server. Please check the "
+                    f"{"PLEX_SERVER_PUBLIC_IP" if self.using_public_ip else "PLEX_SERVER_IP"} "
+                    "in .env, and consult the README."
                 )
         except plexapi.exceptions.Unauthorized:
             sys.exit(
                 'Invalid Plex token. Please check the "PLEX_TOKEN" in .env, and consult the README.'
             )
 
-        try:
-            tv: LibrarySection = plex.library.section(self.tv_library)
-        except plexapi.exceptions.NotFound:
-            sys.exit(
-                f'TV library named "{self.tv_library}" not found. Please check the "PLEX_TV_LIBRARY" in .env, '
-                'and consult the README.'
-            )
-        try:
-            movies: LibrarySection = plex.library.section(self.movie_library)
-        except plexapi.exceptions.NotFound:
-            sys.exit(
-                f'Movie library named "{self.movie_library}" not found. Please check the "PLEX_MOVIE_LIBRARY" '
-                'in .env, and consult the README.'
-            )
+    def get_libraries(self) -> dict[str, LibrarySection]:
+        """
+        Return accessible Plex libraries.
 
-        return tv, movies
+        Returns:
+            dict[str, LibrarySection]: {library name: Plex library object}
+        """
+        plex_libraries: dict[str, LibrarySection] = {}
+        for library in self.libraries:
+            try:
+                plex_libraries[library] = self.plex.library.section(library)
+            except plexapi.exceptions.NotFound:
+                sys.exit(
+                    f'Library named "{library}" not found. Please check the config.yml, and consult the README.'
+                )
+
+        return plex_libraries
+
+def main() -> None:
+    """
+    Function to run script logic.
+    """
+    pcm = PlexCollectionMaker()
+
+    plex_libraries = pcm.get_libraries()
+
+    print("Found Plex libraries: ", end="")
+    print(*plex_libraries.keys(), sep=", ")
+    print("Found collection configs:")
+    for lib in plex_libraries:
+        print(f"  {lib}: ", end="")
+        print(*pcm.collections_config[lib].keys(), sep=", ")
+
+    print()
+    for lib in plex_libraries:
+        print(pcm.collections_config[lib])
+
+    print()
+    print(plex_libraries['Movies'].title)
+    print(plex_libraries["Movies"].recentlyAdded(5))
 
 
 if __name__ == "__main__":
-    pcm = PlexCollectionMaker()
-
-    tv, movies = pcm.plex_setup()
-    print(tv.recentlyAdded(5))
-    print(movies.recentlyAdded(5))
+    main()
